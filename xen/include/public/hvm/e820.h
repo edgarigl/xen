9aa60746945de71e69957114cf842c04a0269de6
Xenia Ragiadakou <xenia.ragiadakou@amd.com>
2025-02-04 10:08:14 +0200

    2 ecam
    Signed-off-by: Xenia Ragiadakou <xenia.ragiadakou@amd.com>
    Change-Id: Iee16feb7ee3cdbe143143bbee858cdba5691fe1f

--- xen/include/public/hvm/e820.h
+++ xen/include/public/hvm/e820.h
@@ -18,4 +18,29 @@
 #define HVM_BELOW_4G_MMIO_LENGTH    ((xen_mk_ullong(1) << 32) - \
                                      HVM_BELOW_4G_MMIO_START)
 
+/* PCI Root Bridges attached to PVH guests */
+#define PCIE_GSI_BASE               (16U)
+
+/* Resources allocated to the vpci root bridge */
+#define PCIE_VPCI_SEG               (0U)
+#define PCIE_VPCI_NR_BUS            (256U)
+#define PCIE_VPCI_ECAM_BASE         xen_mk_ullong(0xe000000000)
+#define PCIE_VPCI_ECAM_SIZE         (PCIE_VPCI_NR_BUS * 0x100000UL)
+#define PCIE_VPCI_MMIO_BASE         xen_mk_ulong(0xf0000000)
+#define PCIE_VPCI_MMIO_SIZE         xen_mk_ulong(0x02000000)
+#define PCIE_VPCI_64BIT_MMIO_BASE   xen_mk_ullong(0xc000000000)
+#define PCIE_VPCI_64BIT_MMIO_SIZE   xen_mk_ullong(0x1000000000)
+#define PCIE_VPCI_INTX_BASE         PCIE_GSI_BASE
+
+/* Resources allocated to the virtio pci root bridge */
+#define PCIE_VIRTIO_SEG             (1U)
+#define PCIE_VIRTIO_NR_BUS          (256U)
+#define PCIE_VIRTIO_ECAM_BASE       xen_mk_ullong(0xe010000000)
+#define PCIE_VIRTIO_ECAM_SIZE       (PCIE_VIRTIO_NR_BUS * 0x100000UL)
+#define PCIE_VIRTIO_MMIO_BASE       xen_mk_ulong(0xf2000000)
+#define PCIE_VIRTIO_MMIO_SIZE       xen_mk_ulong(0x02000000)
+#define PCIE_VIRTIO_64BIT_MMIO_BASE xen_mk_ullong(0xd000000000)
+#define PCIE_VIRTIO_64BIT_MMIO_SIZE xen_mk_ullong(0x1000000000)
+#define PCIE_VIRTIO_INTX_BASE       PCIE_GSI_BASE
+
 #endif /* __XEN_PUBLIC_HVM_E820_H__ */
